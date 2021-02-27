cc7cd0b3a7e1c665bb7d6471d005026730b75212
Roberto Higor <roberto.higor@outlook.com>
2021-02-26 23:43:14 -0300

    Ler arquivo aberto. Método de filtro

--- CsvHelper demo/Program.cs
+++ CsvHelper demo/Program.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
+using System.Text;
 
 namespace CsvHelper_demo
 {
@@ -12,15 +13,28 @@ namespace CsvHelper_demo
     {
         static void Main(string[] args)
         {
-            List<Carro> carros = LerCsv<Carro>("carros.csv");
-            carros.RemoveAt(1);
+            List<Carro> carros = LerCsv<Carro>("carros_utf8.csv");
+            var carrosFiltrado = filtrarFabricante(carros, "Volkswagen");           
+            //carros.RemoveAt(1);
 
-            foreach (Carro carro in carros)
+            foreach (Carro carro in carrosFiltrado)
             {
                 Console.WriteLine(carro.ToString());
             }
 
-            EscreverCsv(carros, "carros.csv");
+            //EscreverCsv<Carro>(carros, "carros.csv");
+        }
+
+        private static List<Carro> filtrarFabricante(List<Carro> carros, string fabricante)
+        {
+            // Apenas carros de determinada marca
+            IEnumerable<Carro> carrosFiltrado = from carro in carros
+                                         where carro.Fabricante == fabricante
+                                         select carro;
+
+            return carrosFiltrado.ToList();
+            // Alternativa
+            //var carros3 = carros.Where(carro => carro.Fabricante == "Volkswagen");
         }
 
         private static List<T> LerCsv<T>(string caminho)
@@ -30,34 +44,52 @@ namespace CsvHelper_demo
                 // Caso as colunas sejam minúsculas no csv e não no código.
                 PrepareHeaderForMatch = args => args.Header.ToLower(),
             };
-
-            using (var reader = new StreamReader(caminho))
-            using (var csv = new CsvReader(reader, config))
+            // Abrindo como apenas leitura
+            // FileShare permite abrir arquivo enquanto outros processos o estão usando para algo
+            using (FileStream stream = File.Open(caminho, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
             {
-                // Retorna um registro por vez
-                // Cuidado: Caso se utilize o .ToList() do LINQ, ele lê o arquivo inteiro na memória
-                try
-                {    
-                    
-                    IEnumerable<T> records = csv.GetRecords<T>();
-                    return records.ToList();
-                }
-                catch (HeaderValidationException ex)
+                using (var reader = new StreamReader(stream))
+                using (var csv = new CsvReader(reader, config))
                 {
-                    Console.WriteLine($"{ex}, Erro no mapeamento dos cabeçalhos");
-                    throw;
+                    // Retorna um registro por vez
+                    // Cuidado: Caso se utilize o .ToList() do LINQ, ele lê o arquivo inteiro na memória
+                    try
+                    {
+                        IEnumerable<T> records = csv.GetRecords<T>();
+                        return records.ToList();
+                    }
+                    catch (System.IO.IOException ex)
+                    {
+                        System.Console.WriteLine(ex + "Erro ao ler o arquivo");
+                        throw;
+                    }
+                    catch (HeaderValidationException ex)
+                    {
+                        Console.WriteLine($"{ex}, Erro no mapeamento dos cabeçalhos");
+                        throw;
+                    }
                 }
             }
         }
 
         // Escreve um novo documento com os dados informados
-        private static void EscreverCsv(IEnumerable<Carro> carros, string caminho)
+        private static void EscreverCsv<T>(IEnumerable<T> lista, string caminho)
         {
-            using (var writer = new StreamWriter(caminho))
-            using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+            try
             {
-                csv.WriteRecords(carros);
+                // True para realizar append
+                using (var writer = new StreamWriter(caminho, true, Encoding.UTF8))
+                using (var csv = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                {
+                    csv.WriteRecords(lista);
+                }
             }
+            catch (System.IO.IOException ex)
+            {
+                System.Console.WriteLine(ex + "Erro ao escrever o arquivo");
+                throw;
+            }
+
         }
     }
 }
