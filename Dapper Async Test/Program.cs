14723c628b143f760b5fd62551c81db9c4b5c0e2
Roberto Higor <roberto.higor@outlook.com>
2021-03-03 09:49:52 -0300

    Método Async utilizando Dapper transactions

--- Dapper Async Test/Program.cs
+++ Dapper Async Test/Program.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading.Tasks;
+using System.Transactions;
 using Dapper;
+using Dapper.Transaction;
 using Microsoft.Data.Sqlite;
 
 namespace Dapper_Async_Test
@@ -29,9 +31,19 @@ namespace Dapper_Async_Test
             stopwatch.Start();
 
             System.Console.WriteLine("Começando a contagem");
-            
+
+            await DapperAsyncNovo(listaNumeros).ConfigureAwait(false);
+            //await DapperAsync(listaNumeros).ConfigureAwait(false);
+
+            stopwatch.Stop();
+            System.Console.WriteLine($"Elapsed Time is {stopwatch.ElapsedMilliseconds}");
+        }
+
+        private static async Task DapperAsync(IList<Numero> listaNumeros)
+        {
             // Inserir lista de objetos de forma assíncrona            
-            using (var conn = new SqliteConnection("Data Source=numeros.sqlite")){
+            using (var conn = new SqliteConnection("Data Source=numeros.sqlite"))
+            {
                 await conn.OpenAsync().ConfigureAwait(false);
                 await using var transaction = await conn.BeginTransactionAsync();
 
@@ -40,6 +52,7 @@ namespace Dapper_Async_Test
                     string sql = "INSERT INTO Numeros (Num1, Num2) VALUES (@Num1, @Num2)";
                     //conn.Execute(sql, listaNumeros);            
                     //TODO: Ver para que serve argumento transaction no dapper. Atualmente está opcional.
+                    // Pode servir para associar transaction no caso de não utilizar BeginTransactionAsync
                     await conn.ExecuteAsync(sql, listaNumeros, transaction: transaction);
 
                     await transaction.CommitAsync().ConfigureAwait(false);
@@ -49,10 +62,35 @@ namespace Dapper_Async_Test
                     await transaction.RollbackAsync().ConfigureAwait(false);
                     throw;
                 }
+            }
         }
 
-        stopwatch.Stop();
-            System.Console.WriteLine($"Elapsed Time is {stopwatch.ElapsedMilliseconds}");
+        private static async Task DapperAsyncNovo(IList<Numero> listaNumeros)
+        {
+            // Inserir lista de objetos de forma assíncrona        
+            //using (var transactionScope = new TransactionScope(TransactionScopeAsyncFlowOption.Enabled))
+            //{
+                using (var conn = new SqliteConnection("Data Source=numeros.sqlite"))
+                {
+                    conn.Open();
+                    using (var transaction = conn.BeginTransaction())
+                    {
+
+                        try
+                        {
+                            string sql = "INSERT INTO Numeros (Num1, Num2) VALUES (@Num1, @Num2)";
+                            //await conn.ExecuteAsync(sql, listaNumeros).ConfigureAwait(false);
+                            await transaction.ExecuteAsync(sql, listaNumeros);
+                            await transaction.CommitAsync();
+                        }
+                        catch (System.Exception)
+                        {
+                            throw;
+                        }
+                    }
+                }
+               // transactionScope.Complete();
+            //}
         }
-}
+    }
 }
